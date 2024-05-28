399523ab80d4be38d92e8c525c86a1054e86a31f
Teten Nugraha <teten.nugraha18@gmail.com>
2024-05-28 13:43:31 +0700

    feat : apply data not found exception

--- src/main/java/id/ten/authapi/exceptions/CustomExceptionHandler.java
+++ src/main/java/id/ten/authapi/exceptions/CustomExceptionHandler.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
+  @ExceptionHandler(DataNotFoundException.class)
+  public GenericResponse<String> handleUserNotFoundException(DataNotFoundException exception) {
+    return GenericResponse.error(exception.getMessage());
+  }
+
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public GenericResponse<Map<String, String>> handleInvalidArgument(
---
+++ src/main/java/id/ten/authapi/exceptions/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package id.ten.authapi.exceptions;
+
+public class DataNotFoundException extends Exception {
+  public DataNotFoundException(String message) {
+    super(message);
+  }
+}
